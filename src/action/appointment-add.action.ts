4d7475c95f906d7c60d997b5fa21a41eedf0fdfe
Ron Penaranda <roncharlespenaranda@gmail.com>
2025-10-04 11:16:36 +0800

    Update appointment-add.action.ts

--- src/action/appointment-add.action.ts
+++ src/action/appointment-add.action.ts
@@ -9,13 +9,16 @@ export const add_appointment = async (body: Appointment) => {
     const patient = await fetch_patient_by_id(body.patient_uid);
     const doctor = await fetch_doctor_by_id(body.doctor_id);
 
+    console.log("dog");
+    console.log(patient);
+
     const now = new Date().toISOString();
 
     const mutateBody = {
       patient_uid: body.patient_uid,
       doctor_id: body.doctor_id,
       patient_name: Array.isArray(patient)
-        ? patient.map((p) => p.first_name).join(", ")
+        ? patient.map((p) => `${p.first_name}`).join(", ")
         : patient ?? "",
       doctor_name: Array.isArray(doctor)
         ? doctor.map((d) => d.name).join(", ")
